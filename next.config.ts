95954ba483d188805ce612dd62b230406fa1d66e
devarogundade <devarogundade@gmail.com>
2026-09-15 07:49:30 +0100

    feat: add Cloudinary to remotePatterns for image optimization

--- next.config.ts
+++ next.config.ts
@@ -10,6 +10,7 @@ const nextConfig: NextConfig = {
     remotePatterns: [
       { protocol: "https", hostname: "images.unsplash.com" },
       { protocol: "https", hostname: "images.pexels.com" },
+      { protocol: "https", hostname: "res.cloudinary.com" },
       { protocol: "https", hostname: "s3-alpha.figma.com" },
       { protocol: "https", hostname: "figma-alpha-api.s3.us-west-2.amazonaws.com" },
     ],
